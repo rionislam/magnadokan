5b48676107fe2cb389bc222aaf9440be9e2188db
rionislam <rionislam.mazidul@gmail.com>
2023-12-25 14:29:48 +0600

    Changed the column name in bookLogs from clientId to clientUid

--- src/Core/Models/Log.php
+++ src/Core/Models/Log.php
@@ -2,7 +2,7 @@
 namespace Core\Models;
 class Log extends Dbh{
     protected function addBookLogs($event, $userIp, $userLogedIn, $userId, $userCountry, $pageUrl, $bookId, $bookCategory){
-        $this->createRow('bookLogs',['event', 'userIp', 'userLoggedIn', 'userId', 'userCountry', 'pageUrl', 'bookId', 'bookCategory', 'clientId'], 'ssiississ', $event, $userIp, $userLogedIn, $userId, $userCountry, $pageUrl, $bookId, $bookCategory, (isset($_COOKIE['CLIENT_ID'])?$_COOKIE['CLIENT_ID']:$_SESSION['CLIENT_ID']));
+        $this->createRow('bookLogs',['event', 'userIp', 'userLoggedIn', 'userId', 'userCountry', 'pageUrl', 'bookId', 'bookCategory', 'clientUid'], 'ssiississ', $event, $userIp, $userLogedIn, $userId, $userCountry, $pageUrl, $bookId, $bookCategory, (isset($_COOKIE['CLIENT_ID'])?$_COOKIE['CLIENT_ID']:$_SESSION['CLIENT_ID']));
     }
 
 }
