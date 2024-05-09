1597b116fe45155591916a242bc83343b08b6fbb
rionislam <rionislam.mazidul@gmail.com>
2024-05-09 20:14:40 +0600

    Upgraded the error handeling of IpInfo utility

--- src/Core/Utilities/IpInfo.php
+++ src/Core/Utilities/IpInfo.php
@@ -5,34 +5,51 @@ class IpInfo{
     private static $ipdatArray = array();
 
     public static function get($ip, $purpose){
-            $output = NULL;
-            $purpose    = str_replace(array("name", "\n", "\t", " ", "-", "_"), '', strtolower(trim($purpose)));
-            $support    = array("city", "country", "continent", "timeZone");
-            if (filter_var($ip, FILTER_VALIDATE_IP) && in_array($purpose, $support)) {
-                if(self::$ipdatArray == NULL){
-                    $ipdat = file_get_contents("http://www.geoplugin.net/json.gp?ip=" . $ip);
-                    if($ipdat !== false){
-                            $ipdat = @json_decode($ipdat);
-                            if($ipdat->geoplugin_status == 200){
-                                self::$ipdatArray = array(
-                                    'city' => $ipdat->geoplugin_city,
-                                    'country' => $ipdat->geoplugin_countryName,
-                                    'continent' => $ipdat->geoplugin_continentName,
-                                    'timeZone' => $ipdat->geoplugin_timezone
-                                );
-                            }else{
-                                self::$ipdatArray = array(
-                                    'city' => 'Unknown City',
-                                    'country' => 'Unknown County',
-                                    'continent' => 'Unknown Continent',
-                                    'timeZone' => 'Unknown TimeZone'
-                                );
-                            }
-                    }
-                }
-                $output = self::$ipdatArray[$purpose];
-            }
+        $output = NULL;
+        $purpose = str_replace(array("name", "\n", "\t", " ", "-", "_"), '', strtolower(trim($purpose)));
+        $support = array("city", "country", "continent", "timeZone");
+        
+        if (filter_var($ip, FILTER_VALIDATE_IP) && in_array($purpose, $support)) {
+            if (self::$ipdatArray == NULL) {
+                $context = stream_context_create([
+                    'http' => [
+                        'timeout' => 5,
+                    ],
+                ]);
+                $ipdat = @file_get_contents("http://www.geoplugin.net/json.gp?ip=" . $ip, false, $context);
                 
-            return $output;
+                if ($ipdat !== false) {
+                    $ipdat = @json_decode($ipdat);
+                    
+                    if ($ipdat->geoplugin_status == 200) {
+                        self::$ipdatArray = array(
+                            'city' => $ipdat->geoplugin_city,
+                            'country' => $ipdat->geoplugin_countryName,
+                            'continent' => $ipdat->geoplugin_continentName,
+                            'timeZone' => $ipdat->geoplugin_timezone
+                        );
+                    } else {
+                        self::$ipdatArray = array(
+                            'city' => 'Unknown City',
+                            'country' => 'Unknown County',
+                            'continent' => 'Unknown Continent',
+                            'timeZone' => 'Unknown TimeZone'
+                        );
+                    }
+                } else {
+                    // Handle API request failure
+                    self::$ipdatArray = array(
+                        'city' => 'Unknown City',
+                        'country' => 'Unknown County',
+                        'continent' => 'Unknown Continent',
+                        'timeZone' => 'Unknown TimeZone'
+                    );
+                }
+            }
+            
+            $output = self::$ipdatArray[$purpose];
+        }
+        
+        return $output;
     }
 }
